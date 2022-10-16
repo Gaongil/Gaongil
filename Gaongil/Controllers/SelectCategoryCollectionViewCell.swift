ea511ab41e25c8f93fd941a7d17e31e1690762f7
lena <lenaminn@gmail.com>
2022-10-16 17:03:44 +0900

    [FEAT] configure selectCategoryCollectionViewCell

---
+++ Gaongil/Controllers/SelectCategoryCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  SelectCategoryCollectionViewCell.swift
+//  Gaongil
+//
+//  Created by Lena on 2022/10/16.
+//
+
+import UIKit
+
+class SelectCategoryCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "SelectCategoryCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(categoryLabel)
+        contentView.backgroundColor = .white
+
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureCellConstraints()
+    }
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .customBlack
+        label.font = .systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private func configureCellConstraints() {
+        NSLayoutConstraint.activate([
+           categoryLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+           categoryLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
