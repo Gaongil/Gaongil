00ef176c6714b95edb7be1572afbecb1dfa2e2e3
lena <lenaminn@gmail.com>
2022-10-16 17:03:57 +0900

    [FEAT] make model of Category

---
+++ Gaongil/Controllers/Category.swift
@@ -0,0 +1,32 @@
+//
+//  Category.swift
+//  Gaongil
+//
+//  Created by Lena on 2022/10/16.
+//
+
+import Foundation
+
+struct Category {
+    var name: String
+}
+
+extension Category {
+    static let categoryNames = [
+        Category(name: "고용"),
+        Category(name: "보건"),
+        Category(name: "기술"),
+        Category(name: "환경"),
+        Category(name: "여성"),
+        Category(name: "법"),
+        Category(name: "금융"),
+        Category(name: "예술"),
+        Category(name: "식품"),
+        Category(name: "교육"),
+        Category(name: "국세"),
+        Category(name: "국방"),
+        Category(name: "외교"),
+        Category(name: "해양")
+        
+    ]
+}
