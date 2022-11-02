4aa21d4e63a4323c69c86e95b96f994f2a3f6f8e
Genesis <dmsgktn1685@naver.com>
2022-11-02 22:55:20 +0900

    [FEAT] CoreData 초기 세팅 및 구현 #12

---
+++ Committee+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Committee+CoreDataClass.swift
+//  Gaongil
+//
+//  Created by ParkJunHyuk on 2022/11/02.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Committee)
+public class Committee: NSManagedObject {
+
+}
---
+++ Committee+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Committee+CoreDataProperties.swift
+//  Gaongil
+//
+//  Created by ParkJunHyuk on 2022/11/02.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Committee {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Committee> {
+        return NSFetchRequest<Committee>(entityName: "Committee")
+    }
+
+    @NSManaged public var isSelected: Bool
+    @NSManaged public var name: String?
+
+}
+
+extension Committee : Identifiable {
+
+}
